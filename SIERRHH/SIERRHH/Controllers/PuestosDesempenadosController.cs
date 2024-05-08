fa91d6b3515d2ccfb135ca71f53adcb544bfc99c
Luis José Villalobos Camareno <39417849+LuisJo125@users.noreply.github.com>
2024-05-08 00:00:42 -0600

    cambios

---
+++ SIERRHH/SIERRHH/Controllers/PuestosDesempenadosController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SIERRHH.Models;
+
+namespace SIERRHH.Controllers
+{
+    public class PuestosDesempenadosController : Controller
+    {
+        private readonly AppBdContext _context;
+
+        public PuestosDesempenadosController(AppBdContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PuestosDesempenados
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PuestosDesempenados.ToListAsync());
+        }
+
+        // GET: PuestosDesempenados/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var puestosDesempenados = await _context.PuestosDesempenados
+                .FirstOrDefaultAsync(m => m.IdPuestoDesempenado == id);
+            if (puestosDesempenados == null)
+            {
+                return NotFound();
+            }
+
+            return View(puestosDesempenados);
+        }
+
+        // GET: PuestosDesempenados/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PuestosDesempenados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPuestoDesempenado,IdEmpleado,Descripcion,Empresa,Tiempo")] PuestosDesempenados puestosDesempenados)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(puestosDesempenados);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(puestosDesempenados);
+        }
+
+        // GET: PuestosDesempenados/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var puestosDesempenados = await _context.PuestosDesempenados.FindAsync(id);
+            if (puestosDesempenados == null)
+            {
+                return NotFound();
+            }
+            return View(puestosDesempenados);
+        }
+
+        // POST: PuestosDesempenados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPuestoDesempenado,IdEmpleado,Descripcion,Empresa,Tiempo")] PuestosDesempenados puestosDesempenados)
+        {
+            if (id != puestosDesempenados.IdPuestoDesempenado)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(puestosDesempenados);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PuestosDesempenadosExists(puestosDesempenados.IdPuestoDesempenado))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(puestosDesempenados);
+        }
+
+        // GET: PuestosDesempenados/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var puestosDesempenados = await _context.PuestosDesempenados
+                .FirstOrDefaultAsync(m => m.IdPuestoDesempenado == id);
+            if (puestosDesempenados == null)
+            {
+                return NotFound();
+            }
+
+            return View(puestosDesempenados);
+        }
+
+        // POST: PuestosDesempenados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var puestosDesempenados = await _context.PuestosDesempenados.FindAsync(id);
+            if (puestosDesempenados != null)
+            {
+                _context.PuestosDesempenados.Remove(puestosDesempenados);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PuestosDesempenadosExists(int id)
+        {
+            return _context.PuestosDesempenados.Any(e => e.IdPuestoDesempenado == id);
+        }
+    }
+}
---
+++ SIERRHH/SIERRHH/Controllers/UsuarioEmpleadoController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SIERRHH.Models;
+
+namespace SIERRHH.Controllers
+{
+    public class UsuarioEmpleadoController : Controller
+    {
+        private readonly AppBdContext _context;
+
+        public UsuarioEmpleadoController(AppBdContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UsuarioEmpleadoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.UsuarioEmpleado.ToListAsync());
+        }
+
+        // GET: UsuarioEmpleadoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioEmpleado = await _context.UsuarioEmpleado
+                .FirstOrDefaultAsync(m => m.IdEmpleado == id);
+            if (usuarioEmpleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuarioEmpleado);
+        }
+
+        // GET: UsuarioEmpleadoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioEmpleadoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdEmpleado,Correo,Password,Rol,Estado")] UsuarioEmpleado usuarioEmpleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuarioEmpleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuarioEmpleado);
+        }
+
+        // GET: UsuarioEmpleadoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioEmpleado = await _context.UsuarioEmpleado.FindAsync(id);
+            if (usuarioEmpleado == null)
+            {
+                return NotFound();
+            }
+            return View(usuarioEmpleado);
+        }
+
+        // POST: UsuarioEmpleadoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEmpleado,Correo,Password,Rol,Estado")] UsuarioEmpleado usuarioEmpleado)
+        {
+            if (id != usuarioEmpleado.IdEmpleado)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuarioEmpleado);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioEmpleadoExists(usuarioEmpleado.IdEmpleado))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuarioEmpleado);
+        }
+
+        // GET: UsuarioEmpleadoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioEmpleado = await _context.UsuarioEmpleado
+                .FirstOrDefaultAsync(m => m.IdEmpleado == id);
+            if (usuarioEmpleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuarioEmpleado);
+        }
+
+        // POST: UsuarioEmpleadoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuarioEmpleado = await _context.UsuarioEmpleado.FindAsync(id);
+            if (usuarioEmpleado != null)
+            {
+                _context.UsuarioEmpleado.Remove(usuarioEmpleado);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioEmpleadoExists(int id)
+        {
+            return _context.UsuarioEmpleado.Any(e => e.IdEmpleado == id);
+        }
+    }
+}
