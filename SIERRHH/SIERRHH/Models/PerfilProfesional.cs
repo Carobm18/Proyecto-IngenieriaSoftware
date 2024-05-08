5e965477297b5f32dfeb431bde2ce10bc4b902c8
Luis José Villalobos Camareno <39417849+LuisJo125@users.noreply.github.com>
2024-05-07 21:28:27 -0600

    ss
    ss

---
+++ SIERRHH/SIERRHH/Models/PerfilProfesional.cs
@@ -0,0 +1,21 @@
+﻿namespace SIERRHH.Models
+{
+    public class PerfilProfesional
+    {
+        public int IdEmpleado { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Dirrecion { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Foto { get; set; }
+    }
+}
